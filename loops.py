bf0b7d6edfdf587ebf220baeca6042003955cb96
tyhiesha-johnson <tyhiesha@lifechoices.co.za>
2025-08-06 11:57:57 +0200

    loops basics

---
+++ loops.py
@@ -0,0 +1,13 @@
+# Basic example of loops in Python
+
+# For loop: printing numbers from 1 to 5
+print("For loop:")
+for i in range(1, 6):
+    print(i)
+
+# While loop: printing numbers from 1 to 5
+print("\nWhile loop:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
