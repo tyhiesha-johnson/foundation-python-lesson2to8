2868a85e5dd3d87df02ace60f7c00be10a4732f6
tyhiesha-johnson <tyhiesha@lifechoices.co.za>
2025-08-06 10:47:34 +0200

    variables basics

---
+++ variables.py
@@ -0,0 +1,16 @@
+# This is a comment
+# Assigning values to variables
+name = "Tyhiesha"
+age = 25
+height = 5.8
+is_student = False
+
+# Printing variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Doing some basic math with variables
+years_until_30 = 30 - age
+print(name, "will be 30 in", years_until_30, "years.")
